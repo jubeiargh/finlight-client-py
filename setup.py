0cd8cd85252bf37fc8dbc3bf4dbacafde90475e3
jubeiargh <buyukkakac@gmail.com>
2025-10-04 15:50:56 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="finlight-client",
-    version="2.0.0",
+    version="2.0.1",
     description="Python client for the Finlight API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
